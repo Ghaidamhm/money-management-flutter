f4f8bc2b8468b9d8ddb2e69879840eb682b57ad3
shathamh1 <shatham.alenzi@gmail.com>
2022-11-02 09:01:54 +0300

    btm&salary&home&history screens done, navgitor done GETx done

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+
+// class ChartScreen extends StatelessWidget {
+//   const ChartScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(
+//         child: Text('setting Screen'),
+//       ),
+//     );
+//   }
+// }
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
